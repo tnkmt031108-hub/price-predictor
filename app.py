3419117622393090941ffdd6afa54abc3061545e
tnkmt031108-hub <tnkmt031108@gmail.com>
2026-02-15 22:39:31 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -72,4 +72,5 @@ def home():
                            result=result)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
